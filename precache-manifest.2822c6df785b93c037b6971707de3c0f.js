62cb330ea1b5fd21112fd3537fad18562eb9d94f
Benjamin Devaublanc <benjamin.devaublanc@gmail.com>
2020-09-19 11:49:42 +0200

    init website

---
+++ precache-manifest.2822c6df785b93c037b6971707de3c0f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b138cb0edfac202dd1a19bd01dab497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d04d4266bf9c4ca8f0",
+    "url": "/static/css/main.f299a513.chunk.css"
+  },
+  {
+    "revision": "040aa4c724046a7025a0",
+    "url": "/static/js/2.400aa8fd.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.400aa8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d04d4266bf9c4ca8f0",
+    "url": "/static/js/main.112ddf75.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "682410c085c674f330d8c64bf5cf91b4",
+    "url": "/static/media/avatar.682410c0.png"
+  },
+  {
+    "revision": "0b60b7d9e631e6f6ae43cd1a50361278",
+    "url": "/static/media/cosurf-previews.0b60b7d9.png"
+  },
+  {
+    "revision": "7813cd8099bb42a16c9d6d25b48970d1",
+    "url": "/static/media/cosurf.7813cd80.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "38269cf047a2c43ec53401c36e617052",
+    "url": "/static/media/foncia-previews.38269cf0.png"
+  },
+  {
+    "revision": "43e74044fe418edae61d43b501786821",
+    "url": "/static/media/ifeelsmart-home.43e74044.png"
+  },
+  {
+    "revision": "4dc03c2f7d5dc38e652834fe46c2a675",
+    "url": "/static/media/ifeelsmart.4dc03c2f.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "12dde2ce7853cf557a33b2acffced359",
+    "url": "/static/media/molotov-home.12dde2ce.png"
+  },
+  {
+    "revision": "832a075f15b4cb9cafa17c72a90f25fa",
+    "url": "/static/media/molotov.832a075f.png"
+  },
+  {
+    "revision": "09901f725cc86850f2735fd535222f33",
+    "url": "/static/media/netvibes-preview.09901f72.png"
+  },
+  {
+    "revision": "dfc4ae98743338fd35d652d49f946f81",
+    "url": "/static/media/netvibes.dfc4ae98.png"
+  },
+  {
+    "revision": "54f8c9d4ee436a6772659a4595ee52c0",
+    "url": "/static/media/popsiit.54f8c9d4.png"
+  }
+]);
